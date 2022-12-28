6c5221cc5b7b23a98e392354dbbe2ce4e9b7dfeb
Riki <dogisriki1122@gmail.com>
2022-12-28 10:20:50 +0900

    シークレットキーが漏れていた

--- youtubepj/settings.py
+++ youtubepj/settings.py
@@ -8,12 +8,11 @@ env.read_env('.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bw5i2gfl#=ru^6rw^p(xid)8ys@t&4bdvoxc@@v&n#u*i#j==0'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
